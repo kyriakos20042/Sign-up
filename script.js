174116d25a78cef8ec79dde7aa12ab159c6c2f90
kyriakos20042 <kyriakospetropoulos04@gmail.com>
2024-03-27 20:35:58 +0200

    Finished

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const registrationForm = document.querySelector("#form");
 const submitButton = document.querySelector("#submit");
 
 const inputPassword = document.querySelector("#pass");
+const inputPasswordConfirm = document.querySelector("#ConfirmPass");
+
 const passwordText = document.querySelector("#passwordCheck");
 
 registrationForm.addEventListener("click", () => {
@@ -13,16 +15,19 @@ registrationForm.addEventListener("click", () => {
             passwordShowText();
         });
     }
+    else {
+        passwordShowDefault();
+    }
 });
 
 function passwordShowText() {
     inputPassword.style.border = "1px solid red";
-    confirmPasswor.style.border = "1px solid red";
+    inputPasswordConfirm.style.border = "1px solid red";
     passwordText.textContent = "* Passwords do not match";
 }
 
 function passwordShowDefault() {
     inputPassword.style.border = "1px solid #ccc";
-    confirmPasswor.style.border = "1px solid #ccc";
+    inputPasswordConfirm.style.border = "1px solid #ccc";
     passwordText.textContent = "";
 }
